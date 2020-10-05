37420bd9136e2799dac5977336c5c05a6ff97b2c
Fabio Nascimento <agsuperpoder@gmail.com>
2020-10-05 13:23:32 +0000

    test  gitpod

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <Meta
-        title="Home"
+        title="Home teste"
         seoTitle="Ecommerce Demo React Next.js"
         seoDescription="Ecommece demo React Next.js Desenvolvimento de sites com tecnologias Modernas"
       />
